a323208ba556f3471312f48e1ec4db78a7e87911
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-12-24 21:04:16 +0545

    feat: add Student controller and initial tests

--- StudentCourseManagement.API/Controllers/StudentsController.cs
+++ StudentCourseManagement.API/Controllers/StudentsController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudentCourseManagement.Business.DTOs.Student;
 using StudentCourseManagement.Business.Interfaces.Services;
+using StudentCourseManagement.Domain.Entities;
 
 namespace StudentCourseManagement.API.Controllers
 {
@@ -8,11 +11,27 @@ namespace StudentCourseManagement.API.Controllers
     public class StudentsController : ControllerBase
     {
         private readonly IStudentService _studentService;
+        private readonly IMapper _mapper;
 
-        public StudentsController(IStudentService studentService)
+        public StudentsController(IStudentService studentService, IMapper mapper)
         {
             this._studentService = studentService;
+            this._mapper = mapper;
         }
+
+
+        #region HttpPost
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] CreateStudentDto createStudentDto)
+        {
+            var student = _mapper.Map<Student>(createStudentDto);
+            await _studentService.Create(student);
+            return Ok("Student Record Added");
+
+
+        }
+        #endregion
 
         #region HttpGet
 
@@ -23,6 +42,53 @@ namespace StudentCourseManagement.API.Controllers
             return Ok(result);
         }
 
+        #region GetById
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> GetByIdAsync([FromRoute] int id)
+        {
+            var student = await _studentService.GetById(id);
+            var studentDto = _mapper.Map<StudentResponseDto>(student);
+
+            return Ok(studentDto);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region HttpPut
+
+        [HttpPut]
+
+        public async Task<IActionResult> UpdateAsync([FromBody] UpdateStudentDto updateStudentDto)
+        {
+            var student = _mapper.Map<Student>(updateStudentDto);
+
+            var isUpdated = await _studentService.Update(student.Id, student);
+
+            if (!isUpdated)
+            {
+                return BadRequest("Update failed");
+            }
+
+            return Ok("Update is successful");
+        }
+        #endregion
+
+
+        #region HttpDelete 
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteAsync([FromRoute] int id)
+        {
+            var isDeleted = await _studentService.Delete(id);
+            if (!isDeleted)
+            {
+                return BadRequest("Failed to delete");
+            }
+            return Ok("Delete is Successful");
+        }
         #endregion
     }
 }
---
+++ StudentCourseManagement.Tests/Controllers/Students/StudentControllerTests_Post.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using StudentCourseManagement.Domain.Entities;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace StudentCourseManagement.Tests.Controllers.Students
+{
+    [TestClass]
+    public class StudentControllerTests_Post
+    {
+        private HttpClient _client;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var factory = new WebApplicationFactory<Program>();
+            _client = factory.CreateClient();
+        }
+
+        [TestMethod]
+        public async Task CreateMember_WithValidData_ReturnOk()
+        {
+            //Arrange 
+            var student = new Student
+            {
+                Name = "Pawan",
+                Address = "Haldibari",
+                Email = "pawan@gmail.com"
+            };
+
+            var response = await _client.PostAsJsonAsync("/api/student", student);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+    }
+}
