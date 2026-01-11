9bcbc6e76b623e5ce0ba6c8886faeaef51e86d9e
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2026-01-11 12:30:55 +0545

    Refactor Student Repository Integration Tests

--- StudentCourseManagement.Tests/Integration/Repositories/StudentRepositoryIntegrationTests.cs
+++ StudentCourseManagement.Tests/Integration/Repositories/StudentRepositoryIntegrationTests.cs
@@ -2,6 +2,7 @@
 using Moq;
 using StudentCourseManagement.Data.Repositories.Dapper;
 using StudentCourseManagement.Domain.Entities;
+using System.Transactions;
 
 namespace StudentCourseManagement.Tests.Integration.Repositories
 {
@@ -18,82 +19,97 @@ namespace StudentCourseManagement.Tests.Integration.Repositories
         [TestMethod]
         public async Task Create_WithValidStudent_InsertRecord()
         {
-            // Arrange
-            var student = new Student
-            {
-                Name = "Ram",
-                Email = "ram.test@test.com",
-                IsActive = true
-            };
+            //Arrange 
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
 
             // Act
-            var result = await _repository.AddAsync(student);
+            var studentId = await CreateStudentAsync();
 
             // Assert
-            Assert.IsNotNull(result);
+            Assert.IsTrue(studentId > 0);
         }
 
         [TestMethod]
         public async Task GetAllAsync_ReturnsListOfStudentData()
         {
+            //Arrange 
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            await CreateStudentAsync();
+            //Act 
+
             var result = await _repository.GetAllAsync();
 
             Assert.IsNotNull(result);
+            Assert.IsTrue(result.Any());
         }
         [TestMethod]
         public async Task GetByIdAsync_WithExisintgId_ReturnsStudentRecord()
         {
-            //let assmune student id 1 exists 
-            int id = 1;
-            var student = await _repository.GetByIdAsync(id);
+            //Arrange 
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+            var studentId = await CreateStudentAsync();
+
+            //Act 
+
+            var student = await _repository.GetByIdAsync(studentId);
+            //Assert 
 
             Assert.IsNotNull(student);
-            Assert.AreEqual(id, student.StudentId);
+            Assert.AreEqual(studentId, student.StudentId);
             Assert.IsTrue(student.IsActive);
         }
-        [TestMethod]
-        public async Task GetByIdAsync_WithNonExisintgId_ReturnNull()
-        {
-            //let assume student id 555 dont exists 
-            int id = 555;
-            var student = await _repository.GetByIdAsync(id);
 
-            Assert.IsNull(student);
-        }
         [TestMethod]
-
         public async Task UpdateAsync_WithExistingId_ReturnsTrue()
         {
-            var id = 1;
-            var student = new Student
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            var studentId = await CreateStudentAsync();
+            var updateStudentData = new Student
             {
-                StudentId = id,
+                StudentId = studentId,
                 Name = "pawan",
                 IsActive = false,
                 Address = " ",
                 Email = "emnail",
-                Gender = "M"
+                Gender = "M",
+                DOB = new DateTimeOffset(2012, 10, 10, 0, 0, 0, TimeSpan.FromHours(3.2)),
+                Number = 9812431234
             };
 
-            var isUpdated = await _repository.UpdateAsync(student);
+            var isUpdated = await _repository.UpdateAsync(updateStudentData);
 
             Assert.IsTrue(isUpdated);
+            var student = await _repository.GetByIdAsync(studentId);
+            Assert.AreEqual(updateStudentData.Name, student.Name);
+            Assert.AreEqual(updateStudentData.Email, student.Email);
         }
 
         [TestMethod]
         public async Task DeleteAsync_WithActiveAndExistingId_ReturnsTrue()
         {
-            int id = 1;
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
 
-            var isDeleted = await _repository.DeleteAsync(id);
+            var studentId = await CreateStudentAsync();
+
+
+            var isDeleted = await _repository.DeleteAsync(studentId);
+
             Assert.IsTrue(isDeleted);
+
+            var student = await _repository.GetByIdAsync(studentId);
+            Assert.IsNull(student);
         }
 
         [TestMethod]
         public async Task IsStudentActiveAsync_WithExistingStudentID_ReturnTrue()
         {
-            int id = 1;
-            var isActive = await _repository.IsStudentActiveAsync(id);
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            var studentId = await CreateStudentAsync();
+
+            var isActive = await _repository.IsStudentActiveAsync(studentId);
 
             Assert.IsTrue(isActive);
         }
@@ -101,10 +117,36 @@ namespace StudentCourseManagement.Tests.Integration.Repositories
         [TestMethod]
         public async Task EmailExistsAsync_IfEmailExists_Returntrue()
         {
-            var email = "emnail";
-            var emailExists = await _repository.EmailExistsAsync(email);
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            var studentId = await CreateStudentAsync();
+
+            var student = await _repository.GetByIdAsync(studentId);
+
+            //Act 
+            var emailExists = await _repository.EmailExistsAsync(student.Email);
+
             Assert.IsTrue(emailExists);
         }
+
+        #region Private Helper Methods 
+
+        private async Task<int> CreateStudentAsync()
+        {
+            var student = new Student
+            {
+                StudentId = 1,
+                Name = "Kiran Sharma",
+                Email = "kiran.sharma@example.com",
+                DOB = new DateTimeOffset(2004, 05, 12, 0, 0, 0, TimeSpan.FromHours(5.75)), // May 12, 2004
+                Number = 9812345678,
+                IsActive = true,
+                Gender = "Female",
+                Address = "Biratnagar, Nepal"
+            };
+            return await _repository.AddAsync(student);
+        }
+        #endregion
 
     }
 }
