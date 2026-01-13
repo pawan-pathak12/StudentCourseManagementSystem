620a7cc7a4cf2e8ff5cee891ffd7f105d164271c
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2026-01-13 11:19:32 +0545

    feat(test): add Invoice service and in-memory repo for CRUD operations

---
+++ StudentCourseManagement.Business/Services/FinancialModule/InvoiceService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Logging;
+using StudentCourseManagement.Business.Interfaces.Repositories.FinancialModule;
+using StudentCourseManagement.Business.Interfaces.Services.FinancialModule;
+using StudentCourseManagement.Domain.Entities.FinancialModule;
+
+namespace StudentCourseManagement.Business.Services.FinancialModule
+{
+    public class InvoiceService : IInvoiceService
+    {
+        private readonly IInvoiceRepository _invoiceRepository;
+        private readonly ILogger<InvoiceService> _logger;
+
+        public InvoiceService(IInvoiceRepository invoiceRepository, ILogger<InvoiceService> logger)
+        {
+            this._invoiceRepository = invoiceRepository;
+            this._logger = logger;
+        }
+
+        #region CURD Operation
+        public async Task<bool> CreateAsync(Invoice invoice)
+        {
+            await _invoiceRepository.AddAsync(invoice);
+            return true;
+        }
+
+        public async Task<bool> DeleteAsync(int invoiceId)
+        {
+            var invoice = await GetByIdAsync(invoiceId);
+            if (invoice == null)
+            {
+                _logger.LogWarning($"Invoice with Id {invoiceId} not found");
+                return false;
+            }
+            return await _invoiceRepository.DeleteAsync(invoiceId);
+        }
+
+        public async Task<IEnumerable<Invoice>> GetAllAsync()
+        {
+            return await _invoiceRepository.GetAllAsync();
+        }
+
+        public async Task<Invoice?> GetByIdAsync(int invoiceId)
+        {
+            return await _invoiceRepository.GetByIdAsync(invoiceId);
+        }
+
+        public async Task<bool> UpdateAsync(int invoiceId, Invoice invoice)
+        {
+            if (invoiceId != invoice.InvoiceId)
+            {
+                _logger.LogWarning("Id mismatched");
+                return false;
+            }
+            return await _invoiceRepository.UpdateAsync(invoiceId, invoice);
+        }
+        #endregion
+
+    }
+}
---
+++ StudentCourseManagement.Data/Repositories/InMemory/financialModule/InMemorryInvoiceRepository.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using StudentCourseManagement.Business.Interfaces.Repositories.FinancialModule;
+using StudentCourseManagement.Domain.Entities.FinancialModule;
+
+namespace StudentCourseManagement.Data.Repositories.InMemory.financialModule
+{
+    public class InMemorryInvoiceRepository : IInvoiceRepository
+    {
+        private readonly List<Invoice> _invoice;
+        private readonly IMapper _mapper;
+
+        public InMemorryInvoiceRepository(IMapper mapper)
+        {
+            _invoice = new List<Invoice>();
+            this._mapper = mapper;
+        }
+
+        #region CURD Operations 
+        public Task<int> AddAsync(Invoice invoice)
+        {
+            _invoice.Add(invoice);
+            invoice.InvoiceId++;
+            return Task.FromResult(invoice.InvoiceId);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var invoice = await GetByIdAsync(id);
+            if (invoice == null)
+            {
+                return false;
+            }
+            invoice.IsActive = false;
+            return true;
+        }
+
+        public Task<IEnumerable<Invoice>> GetAllAsync()
+        {
+            var invoices = _invoice.AsEnumerable();
+            return Task.FromResult(invoices);
+        }
+
+        public Task<Invoice?> GetByIdAsync(int id)
+        {
+            var invoice = _invoice.Find(x => x.InvoiceId == id && x.IsActive == true);
+            return Task.FromResult(invoice);
+        }
+
+        public Task<bool> UpdateAsync(int id, Invoice invoice)
+        {
+            var existingInvoice = _invoice.FirstOrDefault(x => x.InvoiceId == id && x.IsActive == true);
+            if (invoice == null)
+            {
+                return Task.FromResult(false);
+            }
+            _mapper.Map(existingInvoice, invoice);
+            return Task.FromResult(true);
+        }
+        #endregion
+
+    }
+}
