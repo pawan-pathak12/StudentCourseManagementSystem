58c2ca3f9179f4a524ddcea803c66aee626fa36f
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2026-01-24 19:37:13 +0545

    test: added unit test stubs for refund service; in-memory repo and test logic still to be implemented

---
+++ tests/StudentCourseManagement.Tests.Unit/Common/FInacialModules/RefundServiceBaseClass.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Moq;
+using StudentCourseManagement.Business.Mapping;
+using StudentCourseManagement.Business.Mapping.FinancialModule;
+using StudentCourseManagement.Business.Services.FinancialModule;
+using StudentCourseManagement.Data.Repositories.InMemory;
+using StudentCourseManagement.Data.Repositories.InMemory.financialModule;
+using StudentCourseManagement.Data.Repositories.InMemory.FinancialModule;
+
+namespace StudentCourseManagement.Tests.Unit.Common.FInacialModules
+{
+    //
+    public class RefundServiceBaseClass
+    {
+        protected InMemoryPaymentRepository _paymentRepository;
+        protected InMemoryCourseRepository _courseRepository;
+        protected InMemorryInvoiceRepository _invoiceRepository;
+        protected InMemoryFeeAssessmentRepository _feeAssessmentRepository;
+
+        protected RefundService _refundService;
+        [TestInitialize]
+        public void Setup()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CourseProfile>();
+                cfg.AddProfile<PaymentProfile>();
+                cfg.AddProfile<InvoiceProfile>();
+                cfg.AddProfile<FeeAssessmentProfile>();
+            });
+            var mapper = config.CreateMapper();
+            _courseRepository = new InMemoryCourseRepository(mapper);
+            _invoiceRepository = new InMemorryInvoiceRepository(mapper);
+            _feeAssessmentRepository = new InMemoryFeeAssessmentRepository(mapper, _invoiceRepository);
+            _paymentRepository = new InMemoryPaymentRepository(mapper);
+            var mocklogger = new Mock<ILogger<RefundService>>();
+            _refundService = new RefundService(_paymentRepository, _courseRepository, _invoiceRepository, _feeAssessmentRepository, mocklogger.Object);
+        }
+
+    }
+}
---
+++ tests/StudentCourseManagement.Tests.Unit/Services/FinancialModules/Refunds/RefundServiceValidateEligibilityTests.cs
@@ -0,0 +1,39 @@
+﻿using StudentCourseManagement.Tests.Unit.Common.FInacialModules;
+
+namespace StudentCourseManagement.Tests.Unit.Services.FinancialModules.Refunds
+{
+    [TestClass]
+    internal class RefundServiceValidateEligibilityTests : RefundServiceBaseClass
+    {
+        [TestMethod]
+        public async Task ValidateEligibilityAsync_ShouldReturnFalse_WhenPaymentNotFound()
+        {
+        }
+
+        [TestMethod]
+        public async Task ValidateEligibilityAsync_ShouldReturnFalse_WhenPaymentStatusIsNotCompleted()
+        {
+        }
+
+        [TestMethod]
+        public async Task ValidateEligibilityAsync_ShouldReturnFalse_WhenPaymentIsAlreadyRefunded()
+        {
+        }
+
+        [TestMethod]
+        public async Task ValidateEligibilityAsync_ShouldReturnFalse_WhenRefundWindowHasExpired()
+        {
+        }
+
+        [TestMethod]
+        public async Task ValidateEligibilityAsync_ShouldReturnFalse_WhenCourseStartedTooLongAgo()
+        {
+        }
+
+        [TestMethod]
+        public async Task ValidateEligibility_ShouldReturnTrue_WhenAllRulesPass()
+        {
+
+        }
+    }
+}
